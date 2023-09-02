b953d6975c43d6ccdf24f9554c8be1fb288ac0f8
kamalika0363 <59131714+doesnots@users.noreply.github.com>
2023-09-02 21:34:26 +0530

    gradients

--- src/components/AboutSection/Grid.jsx
+++ src/components/AboutSection/Grid.jsx
@@ -2,31 +2,26 @@ import React from "react";
 import "../../globals.css";
 
 const Grid = (props) => {
-  const [effect, setEffect] = React.useState(props.on);
+    const [effect, setEffect] = React.useState(props.on);
 
-  function toggleEnter() {
-    setEffect((prev) => !prev);
-  }
+    function toggleEnter() {
+        setEffect((prev) => !prev);
+    }
 
-  function toggleLeave() {
-    setEffect((prev) => !prev);
-  }
+    function toggleLeave() {
+        setEffect((prev) => !prev);
+    }
 
-  const styles = {
-    backgroundColor: effect ? "red" : "",
-  };
-
-  return (
-    <div
-      className="p-4 border-b"
-      onMouseEnter={toggleEnter}
-      onMouseLeave={toggleLeave}
-      style={styles}
-    >
-      <h1 className="text-xl">{props.title}</h1>
-      <p className="text-sm">{props.description}</p>
-    </div>
-  );
+    return (
+        <div
+            className={`p-4 border-b ${effect ? "custom-gradient" : ""}`}
+            onMouseEnter={toggleEnter}
+            onMouseLeave={toggleLeave}
+        >
+            <h1 className="text-xl">{props.title}</h1>
+            <p className="text-sm">{props.description}</p>
+        </div>
+    );
 };
 
 export default Grid;
