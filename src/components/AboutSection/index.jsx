78e2392a6cd9e2ddf3fac051d8764d61aad05cc9
amit <sutharamit707@gmail.com>
2023-09-03 19:35:34 +0530

    fix css

--- src/components/AboutSection/index.jsx
+++ src/components/AboutSection/index.jsx
@@ -10,22 +10,24 @@ const About = () => {
   ];
 
   return (
-    <section className="bg-[#141619] text-white text-center">
+    <section className="bg-[#141619] text-white text-center min-h-screen p-8 sm:p-16">
       {items.map((item, index) => (
         <div key={index}>
           <h1 className="font-inter text-3xl md:text-3xl lg:text-5xl font-medium">
             {item.title}
           </h1>
-          <p className="mt-6 md:w-2/3 lg:w-2/4 m-auto">{item.paragraph}</p>
+          <p className="mt-6 md:w-2/3 lg:w-2/4 m-auto justify-evenly">
+            {item.paragraph}
+          </p>
         </div>
       ))}
       <div className="mt-8 mx-4 md:mx-5 lg:mx-32 grid grid-cols-2 md:grid-cols-4 gap-2">
         {statsArray.map((item) => (
           <div
             key={item.id}
-            className="p-4 border border-gray-300 hover:primary-gradient rounded"
+            className="p-4 border border-slate-700 hover:primary-gradient rounded"
           >
-            <h2 className="text-md md:text-xl font-semibold">{item.title}</h2>
+            <h2 className="text-md md:text-lg font-semibold">{item.title}</h2>
             <p className="mt-2 text-sm ">{item.description}</p>
           </div>
         ))}
