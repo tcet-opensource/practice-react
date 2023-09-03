d453117f85504b4dc022f93d39679240348b6223
Yashasvi <yashasvivaidya13@gmail.com>
2023-09-03 17:41:33 +0530

    [FEAT] Added Contact US Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Index from "./components/One/index";
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center p-80 font-bold">
-      <p className="text-pink-800">Project 2</p>
+    <div>
+      {/* <p>Project 2</p> */}
       <Index />
     </div>
   );
