1107005feb07ebb46e57da2562ff79d68caecee5
amit <sutharamit707@gmail.com>
2023-09-03 16:07:42 +0530

    fixed box styling

--- src/components/AboutSection/index.jsx
+++ src/components/AboutSection/index.jsx
@@ -9,7 +9,6 @@ const About = () => {
     },
   ];
 
-
   return (
     <section className="bg-[#141619] text-white text-center">
       {items.map((item, index) => (
@@ -17,13 +16,16 @@ const About = () => {
           <h1 className="font-inter text-3xl md:text-3xl lg:text-5xl font-medium">
             {item.title}
           </h1>
-          <p className="mt-6 md:w-2/3 lg:w-2/5 m-auto">{item.paragraph}</p>
+          <p className="mt-6 md:w-2/3 lg:w-2/4 m-auto">{item.paragraph}</p>
         </div>
       ))}
-      <div className="mt-8 grid grid-cols-2 md:grid-cols-4">
+      <div className="mt-8 mx-4 md:mx-5 lg:mx-32 grid grid-cols-2 md:grid-cols-4 gap-2">
         {statsArray.map((item) => (
-          <div key={item.id} className="p-4 border border-gray-300 hover:primary-gradient rounded">
-            <h2 className="text-xl font-semibold">{item.title}</h2>
+          <div
+            key={item.id}
+            className="p-4 border border-gray-300 hover:primary-gradient rounded"
+          >
+            <h2 className="text-md md:text-xl font-semibold">{item.title}</h2>
             <p className="mt-2 text-sm ">{item.description}</p>
           </div>
         ))}
