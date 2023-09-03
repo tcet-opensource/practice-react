23df6976aeda83768955960019ef1409a0a4b560
Alstudd <alstonsoares17@gmail.com>
2023-09-03 22:48:44 +0530

    [ADDED] Exhanges Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Index from "./components/One/index";
+import ExchangeSection from "./components/ExchangeSection";
+
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center font-bold p-80">
-      <p className="text-pink-800">Project 2</p>
-      <Index />
+    <div className="flex flex-col items-center justify-center font-bold mainBG">
+      <ExchangeSection />
     </div>
   );
 }
