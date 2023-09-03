285fb7c17f36da66ce4e1bcbfdee1038d53838c3
pranaytiwariii <pranaytiwariprpk@gmail.com>
2023-09-04 00:25:05 +0530

    [ADDED]footer_section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Index from "./components/One/index";
+import Footer from "./components/Footer/Footer";
+
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center p-80 font-bold">
-      <p className="text-pink-800">Project 2</p>
-      <Index />
-    </div>
+    <>
+    <Footer />
+    </>
   );
 }
 
