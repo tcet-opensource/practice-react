94d8fdc504a20043edefb1e076a5cd35494b2d92
Prachi Vaishya <vaishyaprachi14@gmail.com>
2023-09-04 09:11:20 +0530

    company-slider-section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Index from "./components/One/index";
+import Slider from "./components/Company-Slider";
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center p-80 font-bold">
+   <div className="px-0 lg:px-0 2xl:px-48">
+     {/* <div className="flex flex-col items-center justify-center p-80 font-bold">
       <p className="text-pink-800">Project 2</p>
       <Index />
+    </div> */}
+      <Slider />
     </div>
   );
 }
---
+++ src/components/Company-Slider/CompanySwiper.jsx
@@ -0,0 +1,69 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, A11y, FreeMode } from "swiper/modules";
+
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/free-mode";
+
+ 
+export default function CompanySwiper(){
+  const slideObj = [
+    {
+      id:1,
+      info:"Since 2019, Gravity team has been an astounding market maker for Bitkub. They have proven themselves to be one of the most consistent, committed and driven market makers on our exchange. Gravity Team has contributed high-quality volume and has proven to be very reliable and trustworthy partner. We strongly advocate Gravity Team as they have been an indispensable part of our market-making team."
+    },
+    {
+      id:2,
+      info:"Since 2019, Gravity team has been an astounding market maker for Bitkub. They have proven themselves to be one of the most consistent, committed and driven market makers on our exchange. Gravity Team has contributed high-quality volume and has proven to be very reliable and trustworthy partner. We strongly advocate Gravity Team as they have been an indispensable part of our market-making team."
+    },
+    {
+      id:3,
+      info:"Since 2019, Gravity team has been an astounding market maker for Bitkub. They have proven themselves to be one of the most consistent, committed and driven market makers on our exchange. Gravity Team has contributed high-quality volume and has proven to be very reliable and trustworthy partner. We strongly advocate Gravity Team as they have been an indispensable part of our market-making team."
+    }, 
+    {
+      id:4,
+      info:"Since 2019, Gravity team has been an astounding market maker for Bitkub. They have proven themselves to be one of the most consistent, committed and driven market makers on our exchange. Gravity Team has contributed high-quality volume and has proven to be very reliable and trustworthy partner. We strongly advocate Gravity Team as they have been an indispensable part of our market-making team."
+    },
+    {
+      id:5,
+      info:"Since 2019, Gravity team has been an astounding market maker for Bitkub. They have proven themselves to be one of the most consistent, committed and driven market makers on our exchange. Gravity Team has contributed high-quality volume and has proven to be very reliable and trustworthy partner. We strongly advocate Gravity Team as they have been an indispensable part of our market-making team."
+    },
+  ];
+
+  return (
+    <>
+      <div className="flex justify-center bg-[#333333] mb-6 2xl:mb-9 space-x-4 xl:space-x-8">
+        <div className="swiper-prev w-10 h-10 xl:w-14 xl:h-14 ">
+          <img src="/pagination-default.svg.svg" alt="prev-image-btn" />
+        </div>
+        <div className="swiper-next bg-[#333333] w-10 h-10 xl:w-14 xl:h-14">
+          <img src="/pagination-default.svg.svg" alt="next-image-btn" className="rotate-180 " />
+        </div>
+      </div>
+      <Swiper
+      navigation={{
+        prevEl: ".swiper-prev",
+        nextEl: ".swiper-next",
+      }}
+      modules={[Navigation]}
+      className="mySwiper w-full"
+      spaceBetween={10}
+      slidesPerView = {1}
+    >
+      
+      <div>
+      {slideObj.map((d, i) => (
+        <SwiperSlide key={i} className="font-normal text-center justify-between leading-7 md:leading-8 lg:leading-9 2xl:leading-10 "> 
+          <p >{d.info}</p>
+        </SwiperSlide>
+      ))}
+      </div>
+      
+    </Swiper>
+    
+    </>
+  );
+}
